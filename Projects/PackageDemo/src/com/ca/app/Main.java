8b579d828cdc7a6b415d9a623cfba5b25c7c00d9
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2018-10-16 09:47:00 +0530

    java

---
+++ Projects/PackageDemo/src/com/ca/app/Main.java
@@ -0,0 +1,10 @@
+package com.ca.app;
+
+import com.ca.logic.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        //System.out.println(c.doSum(12.2,14.4));
+    }
+}
